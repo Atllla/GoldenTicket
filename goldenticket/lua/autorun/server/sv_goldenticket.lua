0f8cfc2af5b2b73223012bfbd5a19e830632699a
Atllla <atilla15@gmx.de>
2022-01-18 15:20:22 +0100

    Alkohol-Shop mit den Entities verknüpft
    Entities spawnen nach dem Kaufen

--- goldenticket/lua/autorun/server/sv_goldenticket.lua
+++ goldenticket/lua/autorun/server/sv_goldenticket.lua
@@ -1,5 +1,13 @@
 util.AddNetworkString("Kaufen")
 
+include("entities/whiskey/init.lua")
+include("entities/rum/init.lua")
+include("entities/gin/init.lua")
+include("entities/wodka/init.lua")
+include("entities/rotwein/init.lua")
+
+
+
 concommand.Add("geld", function(ply)
     ply:addMoney(100)
 end)
@@ -19,15 +27,16 @@ end)
 net.Receive("Kaufen", function(len, ply)
     local index = net.ReadInt(5)
     local auswahl = AlkAuswahl.Items[index]
-    print(auswahl.name)
 
     if (ply:canAfford(auswahl.preis)) then
         ply:addMoney(-auswahl.preis)
-        --[[ function AlkSpawn()
-        include("entities/alk/init.lua")
-        ENT:Spawn()
-       end ]]
-    else
-        ply:ChatPrint("Du kannst dir " .. auswahl.name .. " nicht leisten.")
+        function AlkSpawn()
+        entity = ents.Create(auswahl.ent) -- als "entity" wird im Folgenden das Spawnen des Entities bezeichnet
+        spawnpunkt = ply:GetEyeTrace() -- "spawnpunkt" ist dort, wo der Spieler hinschaut
+        entity:Spawn()
+        entity:SetPos(spawnpunkt.HitPos + Vector(0, 0, 10)) -- Das Spawnen des Entities soll 10 Units über dem Punkt sein, wo der Spieler hinschaut
+       end
+
+       AlkSpawn()
     end
 end)
